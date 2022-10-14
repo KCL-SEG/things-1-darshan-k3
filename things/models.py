df4c7519a2cc11126e16960878210554d816ac37
Darshan Kumaresan <darshan.kumaresan@kcl.ac.uk>
2022-10-14 16:04:24 +0100

    TextField to CharField

--- things/models.py
+++ things/models.py
@@ -3,6 +3,6 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator 
 # Create your models here.
 class Thing(models.Model):
-    name = models.TextField(max_length=30, unique=True)
-    description = models.TextField(unique=False, blank=True, max_length=120)
+    name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(unique=False, blank=True, max_length=120)
     quantity = models.IntegerField(unique=False, validators=[MinValueValidator(0), MaxValueValidator(100)])
