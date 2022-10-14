aabacbce1aed0527a9987b4b7fc2e69cd606d727
Darshan Kumaresan <darshan.kumaresan@kcl.ac.uk>
2022-10-14 17:00:03 +0100

    Tests and changes

--- things/models.py
+++ things/models.py
@@ -1,7 +1,8 @@
+from enum import unique
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator 
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 class Thing(models.Model):
-    name = models.CharField(max_length=30, unique=True)
-    description = models.CharField(unique=False, blank=True, max_length=120)
+    name = models.CharField(max_length=30, blank=False, unique=True)
+    description = models.CharField(max_length=120, blank=True, unique=False)
     quantity = models.IntegerField(unique=False, validators=[MinValueValidator(0), MaxValueValidator(100)])
--- things/tests.py
+++ things/tests.py
@@ -1,3 +1,92 @@
 from django.test import TestCase
-
+from django.core.exceptions import ValidationError
+from .models import Thing
 # Create your tests here.
+class ThingsTestCase(TestCase):
+    def setUp(self):
+        self.thing = Thing.objects.create_thing(
+            name="John",
+            description = 'Hi. This is John',
+            quantity=100
+        )
+
+    #~~~~~~~~~UNIT TESTS FOR NAME~~~~~~~~~~~~~~~~
+    
+    def test_name_must_be_unique(self):
+        second_thing = self._create_second_thing()
+        self.thing.name = second_thing.name
+        self._assert_user_is_invalid()
+
+    def test_name_must_not_be_blank(self):
+        self.thing = ''
+        self._assert_user_is_invalid()
+    
+    def test_name_can_be_50_characters_long(self):
+        self.thing.name =  'x' * 50
+        self._assert_user_is_valid()   
+
+    def test_name_cannot_be_over_50_characters_long(self):
+        self.thing.name = 'x' * 51
+        self._assert_user_is_invalid()
+
+
+    #~~~~~~~~~UNIT TESTS FOR DESCRIPTION~~~~~~~~~~~~~~~~
+    
+    def test_description_may_be_unique(self):
+        second_thing = self._create_second_thing()
+        self.thing.description = second_thing.description
+        self._assert_user_is_valid()
+
+    def test_description_may_be_blank(self):
+        self.thing = ''
+        self._assert_user_is_valid()
+    
+    def test_description_can_be_120_characters_long(self):
+        self.thing.description =  'x' * 120
+        self._assert_user_is_valid()   
+
+    def test_description_cannot_be_over_120_characters_long(self):
+        self.thing.description = 'x' * 121
+        self._assert_user_is_invalid()
+
+
+    #~~~~~~~~~UNIT TESTS FOR QUANTITY~~~~~~~~~~~~~~~~
+    
+    def test_quantity_need_not_be_unique(self):
+        second_thing = self._create_second_thing()
+        self.thing.quantity = second_thing.quantity
+        self._assert_user_is_valid()
+        
+    def test_quantity_can_be_0(self):
+        self.thing.quantity = 0
+        self._assert_user_is_valid()   
+
+    def test_quantity_cannot_be_less_than_0(self):
+        self.thing.quantity = -1
+        self._assert_user_is_invalid()   
+
+    def test_quantity_can_be_100(self):
+        self.thing.quantity = 100
+        self._assert_user_is_valid()   
+
+    def test_name_cannot_be_greater_than_100(self):
+        self.thing.quantity = 101
+        self._assert_user_is_invalid()
+    
+    def _assert_user_is_valid(self):
+        try:
+            self.user.full_clean()
+        except(ValidationError):
+            self.fail('Test user should be valid')
+
+    def _assert_user_is_invalid(self):
+        with self.assertRaises(ValidationError):
+            self.user.full_clean()
+
+    def _create_second_thing(self):
+        thing = Thing.objects.create(
+            name= "John",
+            description = 'Hi. This is John.',
+            quantity = 100,
+        )
+        return thing
